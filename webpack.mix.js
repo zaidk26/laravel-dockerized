eecc6c9af7d61aa9b61e42e4d0ae5ba4633d149c
Zaid Kajee <zaid26@gmail.com>
2020-03-28 09:53:52 +0200

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
 
 mix
-   .js('resources/assets/js/app.js', 'public/js/app.js')
-   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
+   .js('resources/js/app.js', 'public/js/app.js')
+   .sass('resources/sass/app.scss', 'public/css/app.css')
 
    .browserSync({
       proxy: 'https://webserver-hr',
